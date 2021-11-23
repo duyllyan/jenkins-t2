2dfe1d655bea59564f09f10c84ebf9f402793295
Duyllyan <duyllyan@gmail.com>
2021-11-23 09:35:21 -0300

    projeto iniciado

---
+++ src/main/java/com/example/jenkinst2/JenkinsT2Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinst2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinst2/JenkinsT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinst2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
